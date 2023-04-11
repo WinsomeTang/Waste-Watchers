ac70366b7c44498ec9b21d847ca49b807b39b9d9
Michael Werbowy <werbowym@sheridan.college.ca>
2023-04-10 21:38:34 -0400

    added basic validity checks

--- WasteWatchers/LogInPage.js
+++ WasteWatchers/LogInPage.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function LogInPage({ navigation }) {
+    const [username, setUsername] = useState('');
+      const [password, setPassword] = useState('');
+      const [error, setError] = useState('');
+
+      const handleLogin = () => {
+        // Check if username exists in the array
+        const user = users.find((user) => user.username === username);
+        if (!user) {
+          setError('Username does not exist');
+          return;
+        }
+
+        // Check if password matches
+        if (user.password !== password) {
+          setError('Incorrect password');
+          return;
+        }
+
+        // Implement actions for login button press
+        console.log('Login button pressed');
+        // You can navigate to other screens or perform other actions here
+      };
+
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Enter your details</Text>
+      <Text style={styles.title}>Welcome Back !</Text>
       <TextInput style={styles.input} placeholder="Username" />
       <TextInput style={styles.input} placeholder="Password" secureTextEntry />
       <TouchableOpacity
@@ -20,6 +45,9 @@ export default function LogInPage({ navigation }) {
     </View>
   );
 }
+
+const users = []; // Array to store user objects
+
 
 const styles = StyleSheet.create({
   container: {
--- WasteWatchers/SignUpPage.js
+++ WasteWatchers/SignUpPage.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function SignUpPage({ navigation }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSignUp = () => {
+    // Check for empty fields
+    if (username === '' || password === '') {
+      setError('Please fill out all fields');
+      return;
+    }
+
+    // Store username and password in an array
+    const user = { username, password };
+    users.push(user); // users is the array to store user objects
+
+    // Implement actions for sign up button press
+    console.log('Join Game! button pressed');
+    // You can navigate to other screens or perform other actions here
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Create your account</Text>
-      <TextInput style={styles.input} placeholder="Username" />
-      <TextInput style={styles.input} placeholder="Password" secureTextEntry />
-      <TouchableOpacity
-        style={styles.signupButton}
-        onPress={() => {
-          // Implement actions for sign up button press
-          console.log('Join Game! button pressed');
-          // You can navigate to other screens or perform other actions here
-        }}
-      >
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      {error !== '' && <Text style={styles.errorText}>{error}</Text>}
+      <TouchableOpacity style={styles.signupButton} onPress={handleSignUp}>
         <Text style={styles.buttonText}>Join game!</Text>
       </TouchableOpacity>
     </View>
   );
 }
+const users = []; // Array to store user objects
+
 
 const styles = StyleSheet.create({
   container: {
@@ -54,5 +81,10 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 14,
+    marginBottom: 10,
   },
 });
