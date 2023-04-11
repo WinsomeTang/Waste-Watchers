cba4545014deabf47424501dd6d037a4421f25a7
Michael Werbowy <werbowym@sheridan.college.ca>
2023-04-10 20:52:30 -0400

    made first entry screen with buttons

--- WasteWatchers/App.js
+++ WasteWatchers/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import * as ScreenOrientation from 'expo-screen-orientation';
 
 async function changeScreenOrientation() {
@@ -10,7 +10,13 @@ export default function App() {
   changeScreenOrientation();
   return (
     <View style={styles.container}>
-      <Text>LANDSCAPE BABY!</Text>
+      <Text style={styles.title}>Waste Watchers</Text>
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.buttonText}>Log In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.signupButton}>
+        <Text style={styles.signupButtonText}>Sign Up</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -22,7 +28,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  loginButton: {
+    width: 200,
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  signupButton: {
+    width: 200,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  signupButtonText: {
+    color: 'black',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
+
 // git add *
 // git commit -m "uwu"
 // git push origin main
