51ebe85d6a0e8330edc770e221c522bcaebb2290
Michael Werbowy <werbowym@sheridan.college.ca>
2023-04-12 21:15:17 -0400

    simple layout gamepage done

--- WasteWatchers/GamePage.js
+++ WasteWatchers/GamePage.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { View, Image, StyleSheet } from "react-native";
 import backgroundImage from "./images/belt_static.png";
+import wheel1Image from "./images/wheel1.png";
+import wheel2Image from "./images/wheel2.png";
+import wheel3Image from "./images/wheel3.png";
+import wheel4Image from "./images/wheel4.png";
+import landfillImage from "./images/Landfill.png";
+import ewasteBinImage from "./images/EwasteBin.png";
+import recyclingBinImage from "./images/RecyclingBin.png";
+import organicsBinImage from "./images/OrganicsBin.png";
 
 const GamePage = ({ navigation }) => {
   React.useLayoutEffect(() => {
@@ -10,6 +18,14 @@ const GamePage = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Image source={backgroundImage} style={styles.backgroundImage} />
+      <Image source={wheel1Image} style={styles.wheelImage} />
+      <Image source={wheel2Image} style={styles.wheelImage} />
+      <Image source={wheel3Image} style={styles.wheelImage} />
+      <Image source={wheel4Image} style={styles.wheelImage} />
+      <Image source={ewasteBinImage} style={styles.binImage} />
+      <Image source={recyclingBinImage} style={styles.binImage} />
+      <Image source={organicsBinImage} style={styles.binImage} />
+      <Image source={landfillImage} style={styles.landfillImage} />
     </View>
   );
 };
@@ -25,6 +41,18 @@ const styles = StyleSheet.create({
     height: "100%",
     resizeMode: "cover",
     position: "absolute",
+  },
+  wheelImage: {
+    position: "absolute",
+    resizeMode: "contain",
+  },
+  binImage: {
+    position: "absolute",
+    resizeMode: "contain",
+  },
+  landfillImage: {
+    position: "absolute",
+    resizeMode: "contain",
   },
 });
 
