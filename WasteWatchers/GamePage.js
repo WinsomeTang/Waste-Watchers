031d9a0603f90559c18010ce3555ffa45684f5e0
Michael Werbowy <werbowym@sheridan.college.ca>
2023-04-13 11:11:10 -0400

    trash item apple can draggable

--- WasteWatchers/GamePage.js
+++ WasteWatchers/GamePage.js
@@ -1,5 +1,4 @@
-import React, {useState} from "react";
-import {useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Image, StyleSheet, Animated, PanResponder } from "react-native";
 import backgroundImage from "./images/belt_static.png";
 import wheel1Image from "./images/wheel1.png";
@@ -12,10 +11,18 @@ import recyclingBinImage from "./images/RecyclingBin.png";
 import organicsBinImage from "./images/OrganicsBin.png";
 import * as Animatable from "react-native-animatable";
 import appleImage from "./images/Apple.png";
-import plasticBottleImage from "./images/Bottle.png";
-//base layout
 
 const GamePage = ({ navigation }) => {
+  const [applePosition, setApplePosition] = useState({ x: 0, y: 0 });
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (event, gesture) => {
+      setApplePosition({ x: gesture.moveX, y: gesture.moveY });
+    },
+    onPanResponderRelease: () => {},
+  });
+
   React.useLayoutEffect(() => {
     navigation.setOptions({ headerShown: false });
   }, [navigation]);
@@ -28,7 +35,7 @@ const GamePage = ({ navigation }) => {
         style={styles.wheel1Image}
         animation="rotate"
         iterationCount="infinite"
-        duration={3000} // You can adjust the duration and other properties as needed
+        duration={3000}
       />
       <Animatable.Image
         source={wheel2Image}
@@ -51,96 +58,20 @@ const GamePage = ({ navigation }) => {
         iterationCount="infinite"
         duration={3000}
       />
-    const position = new Animated.ValueXY();
-    const panResponder = PanResponder.create({
-    onStartShouldSetPanResponder: (evt, gestureState) => true,
-      onPanResponderMove: (evt, gestureState) => {
-        position.setValue({x: gestureState.dx, y: gestureState.dy});
-      },
-      onPanResponderRelease: (evt, gestureState) => {
-      //Muffin is in the like area
-      if (
-        position.x._value < -50 &&
-        position.y._value > height / 2 - 150 &&
-        position.y._value < height / 2
-      ) {
-          Animated.spring(position, {
-            toValue: {x: -width - 10, y: position.y._value},
-            friction: 10,
-          }).start(() => {
-            addLikedMuffin([...likedMuffins, muffins[currentMuffin].name]);
-            if (currentMuffin == muffins.length - 1) setDone(true);
-            else setCurrentMuffin(currentMuffin + 1);
-            position.setValue({x: 0, y: 0});
-          });
-      }
-      // Muffin is in the dislike area
-      else if (
-        position.x._value > 50 &&
-        position.y._value > height / 2 - 150 &&
-        position.y._value < height / 2
-      ) {
-        Animated.spring(position, {
-          toValue: {x: width + 10, y: position.y._value},
-          friction: 10,
-        }).start(() => {
-          addDislikedMuffin([...dislikedMuffins, appleImage[currentMuffin].name]);
-          if (currentMuffin == appleImage.length - 1) setDone(true);
-          else setCurrentMuffin(currentMuffin + 1);
-          position.setValue({x: 0, y: 0});
-        });
-      }
-      //Muffin is neither in the like nor the dislike area
-      else {
-        Animated.spring(position, {
-          toValue: {x: 0, y: 0},
-          friction: 10,
-        }).start();
-      }
-    },
-    });
-
-    <Animatable.Image
-            source={appleImage}
-            style={[
-              styles.appleImage,
-              {
-                transform: [
-                  {
-                    translateX: this.state.pan.x,
-                  },
-                  {
-                    translateY: this.state.pan.y,
-                  },
-                ],
-              },
-            ]}
-            {...this._panResponder.panHandlers}
-    />
 
       <Image source={ewasteBinImage} style={styles.binImage} />
       <Image source={recyclingBinImage} style={styles.binImage} />
       <Image source={organicsBinImage} style={styles.binImage} />
       <Image source={landfillImage} style={styles.landfillImage} />
-      <Image source={appleImage} style={styles.appleImage} />
-      <Image source={plasticBottleImage} style={styles.plasticBottleImage} />
+      <Animated.Image
+        source={appleImage}
+        style={[styles.appleImage, { top: applePosition.y, left: applePosition.x }]}
+        {...panResponder.panHandlers}
+      />
     </View>
   );
-
-    const [likedMuffins, addLikedMuffin] = useState([]);
-    const [dislikedMuffins, addDislikedMuffin] = useState([]);
-    //State for checking the index of the current muffin
-    const [currentMuffin, setCurrentMuffin] = useState(0);
-    //State for checking if all the muffins are done
-    const [done, setDone] = useState(false);
-
-
-
 };
 
-
-
-//Good code start here
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -182,23 +113,27 @@ const styles = StyleSheet.create({
   wheelImage: {
     position: "absolute",
     resizeMode: "contain",
-  },
-  binImage: {
-    position: "absolute",
-    resizeMode: "contain",
+    width: "8%",
+    top: "34%",
+    left: "18%",
   },
   landfillImage: {
     position: "absolute",
     resizeMode: "contain",
   },
+  binImage: {
+    position: "absolute",
+    resizeMode: "contain",
+  },
   appleImage: {
-      position: "absolute",
-      resizeMode: "contain",
-    },
-    plasticBottleImage: {
-      position: "absolute",
-      resizeMode: "contain",
-    }
+    position: "absolute",
+    resizeMode: "contain",
+    width: "10%",
+    height: "10%",
+    top: "10%",
+    left: "10%",
+    zIndex: 999, // Add zIndex to make the apple draggable over other elements
+  },
 });
 
 export default GamePage;
